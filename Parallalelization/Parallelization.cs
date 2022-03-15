dc48c46de7b02e8aaabf8d088c20fe1133daf438
Igor Henriques <lycanium@hotmail.com>
2022-03-15 11:58:43 -0300

    Add project files.

---
+++ Parallalelization/Parallelization.cs
@@ -0,0 +1,96 @@
+﻿namespace Parallalelization
+{
+    public static class Parallelization
+    {
+        public static async Task ParallelizeAsync<T>(this IEnumerable<T> collection, int parallelSize, Func<T, Task> callback, bool descriptiveOperations = false)
+        {
+            Validate(collection, parallelSize, callback);
+
+            var loopTaskList = new List<Task>();
+
+            for (int i = 0; i < parallelSize; i++)
+            {
+                if (descriptiveOperations)
+                    Console.WriteLine($"INITIALIZAING PARALLELIZATION Nº {i}");
+
+                var currentLoop = collection
+                    .Skip(i * collection.Count() / parallelSize)
+                    .Take(collection.Count() / parallelSize)
+                    .Select(task => new { Task = task, Pod = i })
+                    .ToList();
+
+                var loopTask = Task.Run(async () =>
+                {
+                    foreach (var item in currentLoop.Select((model, index) => (model, index)))
+                    {
+                        if (descriptiveOperations)
+                            Console.WriteLine($"POD {item.model.Pod + 1} - PROCESSING TASK INDEX {item.index} OF {currentLoop.Count - 1}");
+
+                        await callback.Invoke(item.model.Task);
+
+                        if (descriptiveOperations)
+                            Console.WriteLine($"POD {item.model.Pod + 1} - TASK INDEX {item.index} PROCESSED. {currentLoop.Count - 1 - item.index} TASKS REMAINING");
+                    }
+                });
+
+                loopTaskList.Add(loopTask);
+            }
+
+            await Task.WhenAll(loopTaskList);            
+        }
+
+        public static void Parallelize<T>(this IEnumerable<T> collection, int parallelSize, Func<T, Task> callback, bool descriptiveOperations = false)
+        {
+            Validate(collection, parallelSize, callback);
+
+            var loopTaskList = new List<Task>();
+
+            for (int i = 0; i < parallelSize; i++)
+            {
+                if (descriptiveOperations)
+                    Console.WriteLine($"INICIALIZANDO PARALELIZAÇÃO Nº {i}");
+
+                var currentLoop = collection
+                    .Skip(i * collection.Count() / parallelSize)
+                    .Take(collection.Count() / parallelSize)
+                    .Select(task => new { Task = task, Pod = i })
+                    .ToList();
+
+                var loopTask = Task.Run(async () =>
+                {
+                    foreach (var item in currentLoop.Select((model, index) => (model, index)))
+                    {
+                        if (descriptiveOperations)
+                            Console.WriteLine($"POD {item.model.Pod + 1} - PROCESSING TASK INDEX {item.index} OF {currentLoop.Count - 1}");
+
+                        await callback.Invoke(item.model.Task);
+
+                        if (descriptiveOperations)
+                            Console.WriteLine($"POD {item.model.Pod + 1} - TASK INDEX {item.index} PROCESSED. {currentLoop.Count - 1 - item.index} TASKS REMAINING");
+                    }
+                });
+
+                loopTaskList.Add(loopTask);
+            }
+
+            Task.WaitAll(loopTaskList.ToArray());
+        }
+
+        private static void Validate<T>(IEnumerable<T> collection, int parallelSize, Func<T, Task> callback)
+        {
+            List<string> errors = new();
+
+            if (collection == null)
+                errors.Add("Collection can't be null");
+
+            if (parallelSize < 2)
+                errors.Add("ParallelSize can't be less than 2");
+
+            if (callback is null)
+                errors.Add("Callback can't be null");
+
+            if (errors.Any())
+                throw new ApplicationException(string.Join("\n", errors));
+        }
+    }
+}
---
+++ Parallalelization/Program.cs
@@ -0,0 +1,21 @@
+﻿using Parallalelization;
+using System.Diagnostics;
+
+using var httpClient = new HttpClient();
+
+var sw = Stopwatch.StartNew();
+
+int parallelSize = 1;
+
+var collection = Enumerable.Range(0, 100).Select(i => new string("https://www.google.com"));
+
+await collection.ParallelizeAsync(parallelSize, async (string url) => await GetAsync(url), true);
+
+Console.WriteLine($"Operation finished in {sw.ElapsedMilliseconds}ms with parallelization of size {parallelSize}");
+
+async Task GetAsync(string url)
+{
+    await httpClient.GetStringAsync(url);
+
+    await Task.Delay(1000);
+}
